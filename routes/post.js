afbd9def8b00e950bd14176537ee33dab6fe586f
Harsh veer singh <harshveer7828@gmail.com>
2023-12-19 12:53:30 +0530

    added a app

---
+++ routes/post.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+const postSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    image:String,
+    title: {
+        type:String,
+    },
+    description: {
+        type: String,
+    },
+    likes: {
+        type: Array,
+        default:[]
+    },
+
+});
+
+// Create the Post model
+module.exports = mongoose.model('Post', postSchema);
+
+
---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const plm=require("passport-local-mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestClone");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique:true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+  post:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Post',
+  }],
+  profileImage:{
+    type:String,
+  }
+});
+
+userSchema.plugin(plm);
+
+module.exports=mongoose.model('User',userSchema);
